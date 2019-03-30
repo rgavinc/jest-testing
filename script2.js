47700ad2d3992cddc5caf7637008c98200c64e54
Personal <personal@Gavins-MacBook-Pro.local>
2019-03-30 12:48:54 -0600

    finished up test 2

--- script2.js
+++ script2.js
@@ -1,12 +1,12 @@
 const fetch = require("node-fetch");
 
-const getPeoplePromise = () => {
+const getPeoplePromise = (fetch = fetch) => {
   return fetch("https://swapi.co/api/people")
     .then(response => response.json())
     .then(({ count, results }) => ({ count, results }));
 };
 
-const getPeople = async () => {
+const getPeople = async (fetch = fetch) => {
   const getRequest = await fetch("https://swapi.co/api/people");
   const { count, results } = await getRequest.json();
   return { count, results };
